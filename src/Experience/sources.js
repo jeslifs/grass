ba69bcef6f5fcc36cb66095d519fce657504dc51
jeslif <jeslif2020@gmail.com>
2025-11-12 18:11:59 +0530

    Xlink, meta, source code, google, favicon

--- src/Experience/sources.js
+++ src/Experience/sources.js
@@ -1,17 +1,17 @@
 export default [
-    {
-        name: 'environmentMapTexture',
-        type: 'cubeTexture',
-        path:
-        [
-            'textures/environmentMap/px.jpg',
-            'textures/environmentMap/nx.jpg',
-            'textures/environmentMap/py.jpg',
-            'textures/environmentMap/ny.jpg',
-            'textures/environmentMap/pz.jpg',
-            'textures/environmentMap/nz.jpg'
-        ]
-    },
+    // {
+    //     name: 'environmentMapTexture',
+    //     type: 'cubeTexture',
+    //     path:
+    //     [
+    //         'textures/environmentMap/px.jpg',
+    //         'textures/environmentMap/nx.jpg',
+    //         'textures/environmentMap/py.jpg',
+    //         'textures/environmentMap/ny.jpg',
+    //         'textures/environmentMap/pz.jpg',
+    //         'textures/environmentMap/nz.jpg'
+    //     ]
+    // },
     {
         name: 'perlinTexture',
         type: 'texture',
